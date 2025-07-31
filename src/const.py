619d7c230acb439f3e424d58e27668f3f2cf4406
thaiminhpv <thaiminhpv@gmail.com>
2025-07-31 08:25:07 +0000

    fix arrow

--- src/const.py
+++ src/const.py
@@ -1,6 +1,9 @@
 TEST_COMMANDS = {
     "graphql-python/graphene": "pytest -rA --continue-on-collection-errors",
-    "arrow-py/arrow": "make test",
+    "arrow-py/arrow": r"""
+sed -i '/^\s*pytest$/s/pytest/pytest -rA --continue-on-collection-errors/' Makefile
+make test
+""",
     "numpy/numpy": "python -m pip install -r requirements/all_requirements.txt\nspin test -v",
     "pytest-dev/pytest": "pytest -rA --continue-on-collection-errors",
     "scipy/scipy": "python dev.py test -v -v",
